6aecb43c7de3c6ed533d831efc97207df5f46d22
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2026-03-23 23:04:26 +0100

    chore: db config

--- bookstore-management-system/pkg/config/app.go
+++ bookstore-management-system/pkg/config/app.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local"
+	d, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
