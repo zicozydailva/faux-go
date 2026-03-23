0b43f9f93472dc6f1ef4efd7661bf1f20b40177f
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2026-03-23 22:20:32 +0100

    chore: schema setup for movies

---
+++ movies-crud/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	fmt.Println("Starting server at port 8082...")
+	if err := http.ListenAndServe(":8082", nil); err != nil {
+		log.Fatal(err)
+	}
+}
