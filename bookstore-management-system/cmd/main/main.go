b13fd4b160bb6a6cced34922125fec0bd9e975e9
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2026-03-23 23:14:38 +0100

    chore: updt route import within root file

--- bookstore-management-system/cmd/main/main.go
+++ bookstore-management-system/cmd/main/main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/zicozydailva/faux-go/bookstore-management-system/pkg/routes"
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
 	fmt.Println("Starting server at port 8083...")
-	if err := http.ListenAndServe(":8083", nil); err != nil {
+	if err := http.ListenAndServe(":8083", r); err != nil {
 		log.Fatal(err)
 	}
 }
